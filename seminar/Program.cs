f11cb40e54a6fdf645667d1fea7a3c6836635267
ALBINA <4aroika@gmail.com>
2024-02-15 12:26:12 +0100

    add task1 lesson2

---
+++ seminar/Program.cs
@@ -0,0 +1,17 @@
+﻿int firstNumber = 5;
+int secondNumber = 6;
+
+if (firstNumber > secondNumber)
+
+{
+    Console.WriteLine ("первое число больше");
+}
+
+else if (firstNumber < secondNumber)
+{
+        Console.WriteLine ("второе число больше\n");
+}
+else
+{
+    Console.WriteLine("числа равны");
+}                    
